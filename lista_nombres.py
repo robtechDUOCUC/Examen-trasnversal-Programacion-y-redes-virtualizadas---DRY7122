25069108ae912424d87c7f365e313228e12aece7
roberto briones <r.briones@duocuc.cl>
2024-06-12 22:48:10 -0400

    Añadir script usuario_duoc_verificar_vln.py

---
+++ lista_nombres.py
@@ -0,0 +1,8 @@
+# lista_nombres.py
+
+# Lista de nombres
+nombres = ["Roberto Briones", "Ricardo Caceres", "Juan Barria"]
+
+# Imprimir cada nombre en la lista
+for nombre in nombres:
+    print(nombre)
