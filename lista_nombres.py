0bb1d8f878e0df0430fd3584668eacfc1a3d70a2
robtechDUOCUC <r.briones.duocuc.cl>
2024-06-13 17:25:14 -0400

    verificador_de_vlan.py

--- lista_nombres.py
+++
@@ -1,8 +0,0 @@
-# lista_nombres.py
-
-# Lista de nombres
-nombres = ["Roberto Briones", "Ricardo Caceres", "Juan Barria"]
-
-# Imprimir cada nombre en la lista
-for nombre in nombres:
-    print(nombre)
